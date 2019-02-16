8724d70a960545af9400455795efb03de895f0cd
rkparel <34177282+rkparel@users.noreply.github.com>
2019-02-16 03:49:20 -0800

    Added login forms/button

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,36 @@
-from app import app
 from flask import render_template
+from app import app
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
 
 @app.route('/')
+
 @app.route('/index')
 def index():
-    user = {'username': 'Will'}
-    return render_template('index.html', title='Home',user=user)
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
